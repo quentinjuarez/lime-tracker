65e07d8d88c9b538bc35bad29c7a9a212e91a2cd
Quentin Juarez <quentin.juarez@getmayday.co>
2026-03-03 23:13:01 +0100

    fix map

--- front/src/components/BikeMap.vue
+++ front/src/components/BikeMap.vue
@@ -105,13 +105,13 @@ const { theme } = useTheme();
 const TILE_DARK =
   'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
 const TILE_LIGHT =
-  'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png';
+  'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
 const tileUrl = computed(() =>
   theme.value === 'light' ? TILE_LIGHT : TILE_DARK,
 );
 const tileAttribution = computed(() =>
   theme.value === 'light'
-    ? '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    ? 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
     : '&copy; OpenStreetMap &copy; CARTO',
 );
 
